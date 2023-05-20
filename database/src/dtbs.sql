2a1226d33530048a1a2e18f1e6122cd339beaf6c
xschahl <xavier.schahl@epitech.eu>
2023-05-20 18:03:18 +0200

    [FIX] Database user

--- database/src/dtbs.sql
+++ database/src/dtbs.sql
@@ -2,23 +2,11 @@ CREATE DATABASE dtbs;
 
 use dtbs;
 
-CREATE TABLE todo
-(
-  id bigint NOT NULL AUTO_INCREMENT,
-  title varchar(255) NOT NULL,
-  description text NOT NULL,
-  created_at datetime NOT NULL DEFAULT NOW(),
-  due_time datetime NOT NULL,
-  status varchar(255) NOT NULL,
-  user_id bigint unsigned NOT NULL,
-  CONSTRAINT t_id PRIMARY KEY (id),
-  CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES user(id)
-);
 CREATE TABLE user
 (
   id bigint NOT NULL AUTO_INCREMENT,
+  username varchar(255) NOT NULL,
   email varchar(255) NOT NULL,
   password varchar(255) NOT NULL,
   created_at datetime NOT NULL DEFAULT NOW(),
-  CONSTRAINT pk_user PRIMARY KEY (id)
 );
