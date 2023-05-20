f40a7749669a45d25307d1fe5cbc2388892d44d7
friize8 <yanis.ktab@epitech.eu>
2023-05-20 18:37:14 +0200

    [ADD] finish shop

--- app/src/app/shop/shop.component.ts
+++ app/src/app/shop/shop.component.ts
@@ -6,10 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shop.component.css']
 })
 export class ShopComponent implements OnInit {
+  public url1: string = "../assets/iphone photo.jpeg";
+  changeImage1(event:any){
+    this.url1 = event.target.src;
+  }
+  public url2: string = "../assets/macbook air.jpg";
+  changeImage2(event:any){
+    this.url2 = event.target.src;
+  }
+  public url3: string = "../assets/apple watch.jpeg";
+  changeImage3(event:any){
+    this.url3 = event.target.src;
+  }
+  public url4: string = "../assets/téléchargement.jpeg";
+  changeImage4(event:any){
+    this.url4 = event.target.src;
+  }
+
 
   constructor() { }
 
   ngOnInit(): void {
   }
-
 }
